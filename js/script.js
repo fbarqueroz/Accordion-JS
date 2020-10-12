4aecfb01361fb21aa48877d5463cec0ddd2e6547
fbarqueroz <fabianbarquero9@gmail.com>
2020-10-12 16:41:04 -0600

    Correcion del css

--- js/script.js
+++ js/script.js
@@ -6,11 +6,12 @@ let accordion = document.getElementsByClassName("accordion_button");
 let i;
 
 for (i = 0; i < accordion.length; i++) {
+    // Esta funcion se activa al hacer 'click'
     accordion[i].addEventListener("click", function(event) {
         let acc = event.currentTarget;
         acc.classList.add("active");
         let panel = acc.nextElementSibling;
-        if (panel.style.display === "block") {
+        if (panel.style.display == "block") {
         panel.style.display = "none";
         } else {
         panel.style.display = "block";
